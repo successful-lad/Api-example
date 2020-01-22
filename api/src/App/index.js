269bf4ff844d5531de84499772b9d57f85ec85c1
egorglebenko <blessedonwebdev@gmail.com>
2020-01-22 12:30:19 +0200

    create boilerplate

---
+++ api/src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DataList from "../data-list";
+
+import './styles.scss';
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <DataList/>
+        </div>
+    );
+};
+
+export default App;
