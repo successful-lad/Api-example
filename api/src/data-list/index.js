830842e638baeb1f274c4b795f62e64776e33cb4
egorglebenko <blessedonwebdev@gmail.com>
2020-01-22 12:24:15 +0200

    create boilerplate

---
+++ api/src/data-list/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.scss";
+
+const DataList = () => {
+
+    return (
+        <div className="data-list">List</div>
+    )
+};
+
+export default DataList;
